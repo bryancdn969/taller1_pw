e0cec9f187b3452c406052d8faeb6a2940c3614f
root <root@localhost.localdomain>
2018-09-26 13:17:20 -0400

    Commit de prueba de Hola mundo

---
+++ taller1pw/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'taller1pw';
+
+  selectedValue: string = "";
+  items = [
+    { value: "0", view: "zero" },
+    { value: "1", view: "one" },
+    { value: "2", view: "Two" }
+  ];
+}
